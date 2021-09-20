736e0215be367ad298bc141d21c81ebce7f189e1
Silvina Colonia <81595987+Chibi88@users.noreply.github.com>
2021-09-20 18:11:56 -0300

    Prueba primer ejercicio

---
+++ 6-04.js
@@ -0,0 +1,15 @@
+let films = ["star wars", "totoro", "rocky", "pulp fiction", "la vida es bella"]
+console.log(films[1]);
+films [0] = films[0].toUpperCase();
+films [1] = films[1].toUpperCase();
+films [2] = films[2].toUpperCase();
+films [3] = films[3].toUpperCase();
+films [4] = films[4].toUpperCase();
+console.log(films);
+
+function pasarAMayusculas (arrayStrings){
+
+    for(let i = 0; i  arrayStrings.lenght; i++){
+        arrayStrings 
+      }
+}
